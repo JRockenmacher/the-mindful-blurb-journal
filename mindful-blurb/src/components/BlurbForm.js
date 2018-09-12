f329ef5267f2fb2b404f0a960583fd58c6e7334f
JRocken <jp.rockenmacher@gmail.com>
2018-09-11 22:22:51 -0600

    played with the entry form a bit

--- mindful-blurb/src/components/BlurbForm.js
+++ mindful-blurb/src/components/BlurbForm.js
@@ -8,7 +8,7 @@ class BlurbForm extends Component {
             title: "",
             topic: "",
             emotion: "",
-            body: "",
+            blurb: "",
             // showForm: false
         }
     }
@@ -27,23 +27,20 @@ class BlurbForm extends Component {
     render() {
         return (
             <div className="container">
-                <Button 
-                className="btn-show-form" 
-                bsStyle="warning"
-                // onClick="showForm"
-                >
-                Cheeto For A Thot
-                </Button>
+                
+
                 <Form horizontal
+                // componentClass={ControlLabel}
                 // className="Container"
                 >
-
                     <FormGroup controlId="formHorizontalTitle">
-                        <Col componentClass={ControlLabel} sm={2}>
+                        <Col 
+                        // componentClass={ControlLabel} 
+                        sm={2}>
                         Title
                         </Col>
                         <Col sm={10}>
-                        <FormControl 
+                        <FormControl
                         type="text"
                         name="title" 
                         value= {this.state.title}
@@ -54,7 +51,9 @@ class BlurbForm extends Component {
                     </FormGroup>
 
                     <FormGroup controlId="formHorizontalTopic">
-                        <Col componentClass={ControlLabel} sm={2}>
+                        <Col 
+                        // componentClass={ControlLabel} 
+                        sm={2}>
                         Topic
                         </Col>
                         <Col sm={10}>
@@ -68,7 +67,9 @@ class BlurbForm extends Component {
                     </FormGroup>
 
                     <FormGroup controlId="formHorizontalTopic">
-                        <Col componentClass={ControlLabel} sm={2}>
+                        <Col 
+                        // componentClass={ControlLabel} 
+                        sm={2}>
                         Emotion
                         </Col>
                         <Col sm={10}>
@@ -81,15 +82,27 @@ class BlurbForm extends Component {
                         </Col>
                     </FormGroup>
 
-                    {/* <label htmlFor="title">Title</label>
-                        <input type="text" name="title" value={this.state.title}/>
-                    <label htmlFor="topic">Topic</label>
-                        <input type="select" name="topic" value={this.state.topic}/>
-                    <label htmlFor="mood">Mood</label>
-                        <input type="select" name="mood" value={this.state.emotion}/>
-                    <label htmlFor="body">Blurb Here!</label>
-                        <textarea name="body" id="body" maxLength="240">{this.state.body}</textarea> */}
+                    <FormGroup controlId="formControlsTextarea">
+                        <ControlLabel>Cheeto For Your Thots</ControlLabel>
+                        <FormControl 
+                        name="blurb"
+                        componentClass="textarea" 
+                        placeholder="What's on your mind, eh Bucko?" 
+                        value={this.state.blurb}
+                        onChange={this.handleChange}
+                        maxLength="240"
+                        />
+                    </FormGroup>
+                    <p>characters: this.state.count/240</p>
                 </Form>
+                <br/>
+                <Button 
+                className="btn-show-form" 
+                bsStyle="warning"
+                // onClick="showForm"
+                >
+                Blurbles For Today
+                </Button>
 
             </div>
         );
